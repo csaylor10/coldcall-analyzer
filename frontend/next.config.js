a91360e31e6dda691a4e230188ff2c22fb6ee379
Abdulrahman Nadeem <ar-nadeem@outlook.com>
2025-05-06 19:27:28 +0500

    Make frontend buildable and build it

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  distDir: 'build',
+  assetPrefix: '/static/',
+
+
   // output: 'export',  <-- REMOVE or comment out this line
+
 };
 
 module.exports = nextConfig;
